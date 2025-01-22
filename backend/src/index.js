c05c53d83dd068b2cb3411e6a0562a9de5b2e638
Lucas <lucasimamuraalves@gmail.com>
2025-01-22 04:33:56 -0300

    Connect with MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
+
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
-import statsRoutes from "./routes/stats.route.js";
+import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
@@ -18,8 +20,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
-app.use("/api/stats", statsRoutes);
+app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
+    connectDB();
 })
