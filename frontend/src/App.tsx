47fbfcd13e145f3a28641a0be71da08653588fc2
Lucas <lucasimamuraalves@gmail.com>
2025-01-31 05:41:55 -0300

    Create Topbar and OAuth components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import HomePage from "./pages/home/HomePage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/auth-callback" element={<AuthCallbackPage />}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/sso-callback"
+          element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+        />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
       </Routes>
     </>
   );
