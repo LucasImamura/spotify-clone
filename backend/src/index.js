b0353455b55a5d91459d9b4de9abf7198a7c7f48
Lucas <lucasimamuraalves@gmail.com>
2025-01-31 19:47:51 -0300

    Call create user endpoint and add cors config

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { clerkMiddleware } from '@clerk/express'
 import fileupload from "express-fileupload";
 import path from "path";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -18,6 +19,13 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT;
+
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+));
 
 app.use(express.json()); // Parses req.body
 app.use(clerkMiddleware()); // Adds auth to req object => req.auth
